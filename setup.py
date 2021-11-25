f604ae61f74f8892956e1e6f438becf2001fd84c
Guang Shi <stefanshi1988@gmail.com>
2021-11-24 21:48:24 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='HippsDimes',
-        version='1.0',
+        version='1.1',
         py_modules=['HippsDimes'],
         install_requires=[
             'Click',

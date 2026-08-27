6e9b009eb9ffca5191a47a5c85f6241e293aeb96
Guang <stefanshi1988@gmail.com>
2026-08-26 22:55:59 -0500

    Export PDHG covariance solver

--- hipps_dimes/core.py
+++ hipps_dimes/core.py
@@ -3,3 +3,6 @@
 from .numerics import *  # noqa: F401,F403
 from .models import *  # noqa: F401,F403
 from .api import *  # noqa: F401,F403
+from .covariance_pdhg import (  # noqa: F401
+    fit_gaussian_noise_covariance_pdhg,
+)
